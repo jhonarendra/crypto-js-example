33244ef07ae109a570a82716bcdc4aa34cf38ecb
jhonarendra <jhonarendra@gmail.com>
2022-07-18 14:07:19 +0800

    fix: bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Fragment, useEffect, useState } from 'react';
 import './App.css';
 import ButtonComp from './components/ButtonComp';
-import CriptoJs from 'crypto-js'// import the react-json-view component
+import CriptoJs from 'crypto-js'
 import ReactJson from 'react-json-view'
 import SwitchComp from './components/SwitchComp';
 import { Tab } from '@headlessui/react';
-import { ClipboardIcon } from '@heroicons/react/outline';
+import { ClipboardCheckIcon, ClipboardIcon } from '@heroicons/react/outline';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
 
@@ -16,6 +18,7 @@ function App() {
   const [isDecryptJson, setIsDecryptJson] = useState(false)
   const [jsonDataEncrypt, setJsonDataEncrypt] = useState({})
   const [jsonDataDecrypt, setJsonDataDecrypt] = useState({})
+  const [dataCopied, setDataCopied] = useState([false, false])
 
   const handleFormChange = (e) => {
     const { name, value } = e.target
@@ -27,6 +30,7 @@ function App() {
     
     const encrypted = CriptoJs.AES.encrypt(form.dataEncrypt, form.passwordEncrypt).toString()
     setEncryptResult(encrypted)
+    setDataCopied([false, false])
   }
 
   const onSubmitDecrypt = (e) => {
@@ -39,24 +43,55 @@ function App() {
       if (isDecryptJson) {
         setJsonDataDecrypt(JSON.parse(decrypted))
       }
+      setDataCopied([false, false])
     } catch (error) {
-      console.log(error)
       setDecryptResult('')
       setJsonDataDecrypt({})
+      toast.error('Cannot decrypt data', {
+        theme: 'colored',
+        autoClose: 1000,
+        hideProgressBar: true
+      })
       return false
     }
   }
 
   const copyToClipboard = (e) => {
     let el = null
+    let newDataCopied = [...dataCopied]
     if (e === 'Encrypt') {
-      el = document.getElementById('encryptedResult')      
+      el = document.getElementById('encryptedResult')
+      newDataCopied[0] = true
     } else {
       el = document.getElementById('decryptedResult')
+      newDataCopied[1] = true
     }
     el.select()
     el.setSelectionRange(0, 99999)
     navigator.clipboard.writeText(el.value)
+
+    setDataCopied(newDataCopied)
+    toast.success('Copied to clipboard', {
+      theme: 'colored',
+      autoClose: 1000,
+      hideProgressBar: true
+    })
+  }
+
+  const clearData = (e) => {
+    const newDataCopied = [...dataCopied]
+    if (e === 'Encrypt') {
+      setForm((values) => ({ ...values, dataEncrypt: '', passwordEncrypt: '' }))
+      setEncryptResult('')
+      setJsonDataEncrypt({})
+      newDataCopied[0] = false
+    } else {
+      setForm((values) => ({ ...values, dataDecrypt: '', passwordDecrypt: '' }))
+      setDecryptResult('')
+      setJsonDataDecrypt({})
+      newDataCopied[1] = false
+    }
+    setDataCopied(newDataCopied)
   }
 
   useEffect(() => {
@@ -145,7 +180,9 @@ function App() {
 
                         {
                           (e === 'Encrypt' && isEncryptJson) ? (
-                            <ReactJson src={jsonDataEncrypt} theme="monokai" />
+                            <div className='max-h-96 overflow-y-auto mt-4'>
+                              <ReactJson src={jsonDataEncrypt} theme="ocean" />
+                            </div>
                           ) : ''
                         }
                         
@@ -167,19 +204,35 @@ function App() {
                     </form>
                     {
                       (e === 'Encrypt' || (e === 'Decrypt' && !isDecryptJson)) ? (
-                        <div className='relative'>
+                        <div
+                          className={`
+                            relative
+                            ${
+                              (
+                                (e === 'Encrypt' && encryptResult === '') ||
+                                (e === 'Decrypt' && decryptResult === '')
+                              ) ? 'hidden' : 'block'
+                            }
+                          `}
+                        >
                           <textarea
                             id={(e === 'Encrypt') ? 'encryptedResult' : 'decryptedResult'}
-                            className='t-form mt-4'
-                            value={(e === 'Encrypt') ? encryptResult : decryptResult}
-                            onChange={null}
+                            className='t-form mt-4 h-48'
+                            defaultValue={(e === 'Encrypt') ? encryptResult : decryptResult}
+                            disabled
                           />
                           <button
                             type="button"
-                            className='absolute top-2 right-2'
+                            className='absolute top-2 right-2 p-2 rounded-md border dark:border-gray-700 dark:bg-gray-800'
                             onClick={() => copyToClipboard(e)}
                           >
-                            <ClipboardIcon className='w-5 h-5' />
+                            {
+                              ((e === 'Encrypt' && dataCopied[0]) || (e === 'Decrypt' && dataCopied[1])) ? (
+                                <ClipboardCheckIcon className='w-5 h-5 text-white' />
+                              ) : (
+                                <ClipboardIcon className='w-5 h-5 text-gray-500' />
+                              )
+                            }
                           </button>
                         </div>
                       ) : ''
@@ -188,19 +241,38 @@ function App() {
                     {
                       (e === 'Decrypt' && isDecryptJson) ? (
                         <div className='max-h-96 overflow-y-auto mt-4'>
-                          <ReactJson src={jsonDataDecrypt} theme="monokai" />
+                          <ReactJson src={jsonDataDecrypt} theme="ocean" />
                         </div>
                       ) : ''
                     }
+
+                    {
+                      (
+                        (e === 'Encrypt' && encryptResult === '') ||
+                        (e === 'Decrypt' && decryptResult === '')
+                      ) ? '' : (
+                        <ButtonComp
+                          btnType="button"
+                          btnColor="red"
+                          btnText="Clear"
+                          btnStyle=" w-full text-center mt-4 "
+                          handleClick={() => clearData(e)}
+                        />
+                      )
+                    }
+                    
                   </div>
                 </Tab.Panel>
               ))
             }
           </Tab.Panels>
         </Tab.Group>
+        
       </div>
+      <ToastContainer />
       <div className='hidden'>
         <button className="  relative  rounded-md  shadow-sm  font-medium  text-sm  uppercase  transition-allpx-4 py-2     text-white      focus:outline-none    focus:ring-2    focus:ring-offset-2    focus:ring-brand-500      bg-brand-600    dark:bg-brand-800    hover:bg-brand-500    hover:dark:bg-brand-700  w-full text-center " type="submit"><span className="              ">Encrypt</span></button>
+        <button className=" relative rounded-md shadow-sm font-medium text-sm uppercase transition-allpx-4 py-2    text-white    focus:outline-none   focus:ring-2   focus:ring-offset-2   focus:ring-red-500    bg-red-600   dark:bg-red-800   hover:bg-red-500   hover:dark:bg-red-700  w-full text-center" type="button"><span className="            ">Clear</span></button>
       </div>
     </>
   );
