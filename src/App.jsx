3545f71d1073cd6fea4a92968adb7819b6687f1d
jhonarendra <jhonarendra@gmail.com>
2022-07-18 12:57:39 +0800

    fix: pakai tab

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import './App.css';
 import ButtonComp from './components/ButtonComp';
 import CriptoJs from 'crypto-js'// import the react-json-view component
 import ReactJson from 'react-json-view'
 import SwitchComp from './components/SwitchComp';
+import { Tab } from '@headlessui/react';
 
 function App() {
 
@@ -34,8 +35,11 @@ function App() {
       let decrypted = CriptoJs.AES.decrypt(form.dataDecrypt, form.passwordDecrypt)
       decrypted = decrypted.toString(CriptoJs.enc.Utf8)
       setDecryptResult(decrypted)
-      setJsonDataDecrypt(JSON.parse(decrypted))
+      if (isDecryptJson) {
+        setJsonDataDecrypt(JSON.parse(decrypted))
+      }
     } catch (error) {
+      console.log(error)
       setDecryptResult('')
       setJsonDataDecrypt({})
       return false
@@ -52,6 +56,7 @@ function App() {
         console.log('error', err)
       }
     }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isEncryptJson])
 
   
@@ -65,111 +70,131 @@ function App() {
         console.log('error', err)
       }
     }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isDecryptJson])
   return (
     <>
-      <div className='dark:bg-gray-900 dark:text-white min-h-screen grid grid-cols-1 sm:grid-cols-2 sm:gap-4'>
-        <div className='p-4'>
-          <h3 className='font-medium uppercase py-4'>Encrypt</h3>
-          <form onSubmit={onSubmitEncrypt}>
-            <div>
-              <div className='mb-4'>
-                <SwitchComp
-                  enabled={isEncryptJson}
-                  setEnabled={setIsEncryptJson}
-                />
-                <span className='ml-2'>
-                  Show input as JSON
-                </span>
-              </div>
-              {
-                (!isEncryptJson) ? (
-                  <textarea
-                    className='t-form mb-4 h-48'
-                    name="dataEncrypt"
-                    value={form.dataEncrypt || ''}
-                    onChange={handleFormChange}
-                    placeholder="Data to Encrypt"
-                    required
-                  ></textarea>
-                ) : (
-                  <ReactJson src={jsonDataEncrypt} theme="monokai" />
-                )
-              }
-              
-              <input
-                className='t-form mb-4'
-                type="text"
-                name="passwordEncrypt"
-                value={form.passwordEncrypt || ''}
-                onChange={handleFormChange}
-                placeholder="Password"
-                required
-              />
-              <ButtonComp
-                btnType="submit"
-                btnText="Encrypt"
-                btnStyle=" w-full text-center"
-              />
-            </div>
-          </form>
-          <div
-            className='mt-4 p-4 border dark:border-gray-700 break-words'
-          >
-            { encryptResult }
-          </div>
-        </div>
-        <div className='p-4'>
-          <h3 className='font-medium uppercase py-4'>Decrypt</h3>
-          <div className='mb-4'>
-            <SwitchComp
-              enabled={isDecryptJson}
-              setEnabled={setIsDecryptJson}
-            />
-            <span className='ml-2'>
-              Show output as JSON
-            </span>
-          </div>
-          <form onSubmit={onSubmitDecrypt}>
-            <div>
-              <textarea
-                className='t-form mb-4 h-48'
-                name="dataDecrypt"
-                value={form.dataDecrypt || ''}
-                onChange={handleFormChange}
-                placeholder="Data to Decrypt"
-                required
-              ></textarea>
-              <input
-                className='t-form mb-4'
-                type="text"
-                name="passwordDecrypt"
-                value={form.passwordDecrypt || ''}
-                onChange={handleFormChange}
-                placeholder="Password"
-                required
-              />
-              <ButtonComp
-                btnType="submit"
-                btnText="Decrypt"
-                btnStyle=" w-full text-center "
-              />
-            </div>
-          </form>
-          {
-            (!isDecryptJson) ? (
-              <div
-                className='mt-4 p-4 border dark:border-gray-700 break-words'
-              >
-                { decryptResult }
-              </div>
-            ) : (
-              <div className='max-h-96 overflow-y-auto mt-4'>
-                <ReactJson src={jsonDataDecrypt} theme="monokai" />
-              </div>
-            )
-          }
-        </div>
+      <div className='dark:bg-gray-900 dark:text-white min-h-screen'>
+        <Tab.Group>
+          <Tab.List>
+            {
+              ['Encrypt', 'Decrypt'].map((e) => (
+                <Tab key={e} as={Fragment}>
+                  {({ selected }) => (
+                    <button
+                      type="button"
+                      className={`
+                        p-4
+                        ${selected ? 'border-b-2 border-brand-500' : 'border-b-2 border-transparent'}
+                      `}
+                    >
+                      { e }
+                    </button>
+                  )}
+                </Tab>
+              ))
+            }
+            
+          </Tab.List>
+          <Tab.Panels>
+            {
+              ['Encrypt', 'Decrypt'].map((e) => (
+                <Tab.Panel
+                  key={e}
+                >
+                  <div className='p-4'>
+                    <h3 className='font-medium uppercase py-4'>
+                      { e }
+                    </h3>
+                    <form onSubmit={(e === 'Encrypt') ? onSubmitEncrypt : onSubmitDecrypt}>
+                      <div>
+                        <div className='mb-4'>
+                          <SwitchComp
+                            enabled={(e === 'Encrypt') ? isEncryptJson : isDecryptJson}
+                            setEnabled={(e === 'Encrypt') ? setIsEncryptJson : setIsDecryptJson}
+                          />
+                          <span className='ml-2'>
+                            Show { (e === 'Encrypt') ? 'input' : 'output' } as JSON
+                          </span>
+                        </div>
+                        {
+                          (e === 'Encrypt' && !isEncryptJson) ? (
+                            <textarea
+                              className='t-form mb-4 h-48'
+                              name="dataEncrypt"
+                              value={form.dataEncrypt || ''}
+                              onChange={handleFormChange}
+                              placeholder="Data to Encrypt"
+                              required
+                            ></textarea>
+                          ) : ''
+                        }
+                        {
+                          (e === 'Encrypt' && isEncryptJson) ? (
+                            <ReactJson src={jsonDataEncrypt} theme="monokai" />
+                          ) : ''
+                        }
+                        {
+                          (e === 'Decrypt') ? (
+                            <textarea
+                              className='t-form mb-4 h-48'
+                              name="dataDecrypt"
+                              value={form.dataDecrypt || ''}
+                              onChange={handleFormChange}
+                              placeholder="Data to Decrypt"
+                              required
+                            ></textarea>
+                          ) : ''
+                        }
+                        
+                        <input
+                          className='t-form mb-4'
+                          type="text"
+                          name={(e === 'Encrypt') ? 'passwordEncrypt' : 'passwordDecrypt'}
+                          value={(e === 'Encrypt') ? form.passwordEncrypt : form.passwordDecrypt}
+                          onChange={handleFormChange}
+                          placeholder="Password"
+                          required
+                        />
+                        <ButtonComp
+                          btnType="submit"
+                          btnText={(e === 'Encrypt') ? 'Encrypt' : 'Decrypt'}
+                          btnStyle=" w-full text-center"
+                        />
+                      </div>
+                    </form>
+                    {
+                      (e === 'Encrypt') ? (
+                        <div
+                          className='mt-4 p-4 border dark:border-gray-700 break-words'
+                        >
+                          { encryptResult }
+                        </div>
+                      ) : ''
+                    }
+
+                    {
+                      (e === 'Decrypt' && !isDecryptJson) ? (
+                        <div
+                          className='mt-4 p-4 border dark:border-gray-700 break-words'
+                        >
+                          { decryptResult }
+                        </div>
+                      ) : ''
+                    }
+                    {
+                      (e === 'Decrypt' && isDecryptJson) ? (
+                        <div className='max-h-96 overflow-y-auto mt-4'>
+                          <ReactJson src={jsonDataDecrypt} theme="monokai" />
+                        </div>
+                      ) : ''
+                    }
+                  </div>
+                </Tab.Panel>
+              ))
+            }
+          </Tab.Panels>
+        </Tab.Group>
       </div>
       <div className='hidden'>
       <button className="
